c7e7099466a42f4977a135fa95a2a6a126b9d0c2
hs96wings <hs96wings@gmail.com>
2023-01-30 15:26:50 +0900

    커넥션풀 추가

--- models/index.js
+++ models/index.js
@@ -6,7 +6,17 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+    config.database, config.username, config.password, config,
+    {
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+);
 
 db.Chat = Chat;
 db.sequelize = sequelize;
